7e648b894dc3fe0b0790d78001d0bfd2d2f6b97c
SoaringDylan <dylan@fampiera.nl>
2016-12-15 18:59:34 +0100

    Fixing typo #2
    Apperently git decided to not upload this :)

---
+++ src/sd/bots/discord/mafiabot/modules/Player.java
@@ -0,0 +1,26 @@
+package sd.bots.discord.mafiabot.modules;
+
+import net.dv8tion.jda.core.entities.Member;
+
+/**
+ * Used to store all information required for the game of Mafia +
+ * the JDA Member class for each player.
+ */
+public class Player {
+
+    private Member JDAmember;
+    private String name;
+
+    public Player(Member JDAmember) {
+        this.JDAmember = JDAmember;
+        this.name = JDAmember.getUser().getName();
+    }
+
+    public Member getJDAmember() {
+        return JDAmember;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
